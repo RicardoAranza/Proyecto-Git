3c40b7c98b7a28a8fd1864a7ccd8e8ed47d13d2a
Abelitro <abelitrotemporal47@gmail.com>
2023-10-15 22:54:39 -0600

    En esta fase solamente se crea el main y el menú

---
+++ CalcularAreas.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    int opcion;
+    do {
+        cout << "Calculadora de Areas" << endl;
+        cout << "1. Calcular el area de un cuadrado" << endl;
+        cout << "2. Calcular el area de un triangulo" << endl;
+        cout << "3. Calcular el area de un circulo" << endl;
+        cout << "4. Calcular el area de un rectángulo" << endl;
+        cout << "5. Salir" << endl;
+        cout << "Elija una opcion: ";
+        cin >> opcion;
+        
+        //Aquí iniciará el switch
+        
+        
+        
+        
+        system("cls");
+    } while (opcion != 5);
+
+    return 0;
+}
